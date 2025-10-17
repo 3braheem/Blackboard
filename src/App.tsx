eb5e95658e624a426d6fb6844702767a41bd3cca
3braheem <ibraahimsiri@gmail.com>
2025-10-17 16:12:02 -0400

    Init with Vite

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Blackboard</h1>
+      <div className="card">
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
